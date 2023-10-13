1e32336453d185ca3c2fdd2c9856aa0ebbebe884
R4R3-M37 <iw4narte44@yandex.ru>
2023-10-13 10:59:16 +0500

    • :sparkles: (icon): Add ui icons

---
+++ src/shared/icons/left-arrow.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export const LeftArrow = () => {
+	return (
+		<svg
+			width={35}
+			height={35}
+			fill='none'
+			xmlns='http://www.w3.org/2000/svg'
+		>
+			<path
+				d='M4.04 12.827L24.642 2.346c4.888-2.492 10.203 2.543 7.732 7.32l-2.88 5.566a5.332 5.332 0 000 4.915l2.88 5.567c2.47 4.776-2.844 9.793-7.732 7.32L4.04 22.551a5.41 5.41 0 010-9.725z'
+				strokeWidth={2}
+				strokeLinecap='round'
+				strokeLinejoin='round'
+			/>
+		</svg>
+	)
+}
---
+++ src/shared/icons/mirroring-screen.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export const MirroringScreen = () => {
+	return (
+		<svg
+			width={35}
+			height={35}
+			fill='none'
+			xmlns='http://www.w3.org/2000/svg'
+		>
+			<path
+				d='M1.012 11.095V9.412C1.012 4.365 4.209 1 9.004 1h15.984c4.795 0 7.992 3.365 7.992 8.412v13.46c0 5.047-3.197 8.412-7.992 8.412H23.39M3.713 15.654c7.385.993 12.803 6.713 13.762 14.486M2.003 21.307c5.419.724 9.398 4.93 10.102 10.633M.98 27.683c2.701.37 4.7 2.457 5.05 5.317'
+				strokeWidth={2}
+				strokeLinecap='round'
+				strokeLinejoin='round'
+			/>
+		</svg>
+	)
+}
